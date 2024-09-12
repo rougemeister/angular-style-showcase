4c3b691dd7ab3d43106ceb44dfd0e2cdc8fb805b
rougemeister <rougemeister1@gmail.com>
2024-09-12 13:38:51 +0000

    Added images and updated theme colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeTogglerComponent } from "./components/theme-toggler/theme-toggler.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { HeroComponent } from './components/hero/hero.component';
+import { CarsComponent } from "./components/cars/cars.component";
+import { CarDataService } from '../services/data.service';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ThemeTogglerComponent],
+  imports: [RouterOutlet, ThemeTogglerComponent, HeaderComponent, HeroComponent, CarsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-style-showcase';
+
+  cars: any[] = [];
+
+  constructor(private carDataService: CarDataService) {}
+
+  ngOnInit(): void {
+    this.carDataService.fetchCars().subscribe(data => {
+      this.cars = data;
+      console.log(this.cars);
+    });
+  }
+
+
+  
 }
